0126c8413ff2f47427d733bbbcfea6e061a514d4
Michal Slepko <michal.slepko@gmail.com>
2022-12-05 11:13:06 +0000

    Advent of Code 2022 Day 4

---
+++ Day4/4.php
@@ -0,0 +1,35 @@
+<?php
+
+$input = file(__DIR__ . "/4input.txt");
+
+$fullyContains = 0;
+$overlaps = 0;
+
+foreach($input as $line) {
+	$pairs = explode(',', trim($line));
+	$range1 = explode('-', $pairs[0]);
+	$range2 = explode('-', $pairs[1]);
+	$pair1 = range($range1[0], $range1[1]);
+	$pair2 = range($range2[0], $range2[1]);
+	echo "Pair1: $pairs[0] - Pair2: $pairs[1] \n";
+	
+	if (
+		empty(array_diff($pair1, $pair2)) 
+		|| empty(array_diff($pair2, $pair1))
+		) {
+		$fullyContains++;
+	}
+
+	if (
+		!empty(array_intersect($pair1, $pair2)) 
+		|| !empty(array_intersect($pair2, $pair1))
+		) {
+		$overlaps++;
+	}
+
+	//if array_intersect($pair1, $pair2));
+	//die();
+}
+
+echo "Part1 - $fullyContains \n";
+echo "Part2 - $overlaps \n";
